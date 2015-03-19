31ce4255db78b25ab738a824a58cfe0537fb596c
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-03-19 19:46:12 +0100

    Doc FTPclient et FTPCommandes

--- src/main/java/com/example/ftpClient/FTPClient.java
+++ src/main/java/com/example/ftpClient/FTPClient.java
@@ -8,39 +8,82 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * @author Anne-Sophie et Thibault
+ */
 
+/**
+ * Classe FTPClient
+ */
 public class FTPClient{
 	
+	/**
+	 * Attributs d'instance.
+	 */
+	
+	/** 
+	 * Socket de connexion
+	 */
 	private Socket co;
+	/** 
+	 * Port de connexion
+	 */
 	private int port = 9999;
+	
+	/** 
+	 * Adresse locale
+	 */
 	private String addesse = "127.0.0.1";
+	/** 
+	 * Lecture
+	 */
 	private BufferedReader in;
+	/** 
+	 * Ecriture
+	 */
 	private DataOutputStream out;
+	
+	/** 
+	 * Commandes FTP
+	 */
 	private FTPCommandes commandes;
-
+	
+	
+	
+	
+	/** 
+	 * Constructeur
+	 */
 	public FTPClient() throws UnknownHostException, IOException{
+		
+		// Socket de connexion avec pour paramètres adresse (127.0.0.1) et port.
 		this.co = new Socket(InetAddress.getByName(addesse),port);
 		
+		// Lecture
 		in = new BufferedReader(new InputStreamReader(co.getInputStream()));
+		//Ecriture
 		out = new DataOutputStream(co.getOutputStream());
 		
+		// Pour "attraper" le premier code de connexion (220)
 		String s = in.readLine();
-		
-		
 		System.out.println(s);
-		
-		commandes= new FTPCommandes(in, out);
-		
-		
-		
-		
+		commandes= new FTPCommandes(in, out);	
 	}	
 	
+	/**
+	 * Accesseur commandes
+	 * @return FTPCommandes
+	 */
 	public FTPCommandes getCommandes(){
 		return this.commandes;
 	}
 	
-	
+	/**
+	 * Main, creation d'un FTPClient
+	 * @param args
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
 	public static void main (String args[]) throws UnknownHostException, IOException{
 		
 		@SuppressWarnings("unused")
--- src/main/java/com/example/ftpClient/FTPCommandes.java
+++ src/main/java/com/example/ftpClient/FTPCommandes.java
@@ -12,45 +12,54 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
+
+/**
+ * @author Anne-Sophie et Thibault
+ */
+
+/**
+ * Classe FTPCommandes
+ */
 public class FTPCommandes {
 
+	/**
+	 * Lecture
+	 */
 	private BufferedReader in;
+	/**
+	 * Ecriture
+	 */
 	private DataOutputStream out;
+	/**
+	 * Dossier courant
+	 */
 	private String currentLoc ;
 
+	
+	
+	/**
+	 * Constructeur FTPCommandes 
+	 * @param in lecture
+	 * @param out ecriture
+	 */
 	public FTPCommandes(BufferedReader in, DataOutputStream out) {
 		this.in=in;
 		this.out=out;
 		this.currentLoc ="data";
 	}
-
-
-	public void CMDUSER(String s) throws IOException{
-		out.write(new String("USER "+s+"\n").getBytes());
-		read("331");
-
-
-
-	}
-
-
-	public boolean CMDPASS(String s) throws IOException{
-		out.write(new String("PASS "+s+"\n").getBytes());
-		
-		if(!read("230")){
-			return false;
-		}
-		
-		return true;
-
-
-	}
+	
+	
+	/**
+	 * Permet de lire le code retour du serveur FTP.
+	 * @arg code, le code de retour
+	 * @return vrai si le code n'est pas 550 ou 430 et qu'il a été lu correctement.
+	 */
 	public boolean read(String code) throws IOException{
 		String s = "";
-		boolean codeOK=false;
+		boolean codeOK = false;
 
 		while(!codeOK){
-			s=in.readLine();
+			s = in.readLine();
 			if(s != null){
 				if(s.equals(code)){
 					codeOK=true;
@@ -59,7 +68,7 @@ public class FTPCommandes {
 				}
 				
 				if(s.equals("550") || s.equals("430") ){
-					codeOK=true;
+					codeOK = true;
 					System.out.println(s);
 					return false;
 				}
@@ -69,13 +78,64 @@ public class FTPCommandes {
 		}
 		return true;
 	}
+	
 
+
+	/**
+	 * Commande utilisateur
+	 * Envoie USER au serveur FTP. 
+	 * Attend de lire 331
+	 * @param s Nom de l'utilisateur
+	 */
+	public void CMDUSER(String s) throws IOException{
+		out.write(new String("USER "+s+"\n").getBytes());
+		read("331");
+	}
+
+
+	/**
+	 * Commande password
+	 * Envoie PASS au serveur FTP. 
+	 * Attend de lire 230
+	 * @param s Mot de passe de l'utilisateur
+	 */
+	public boolean CMDPASS(String s) throws IOException{
+		out.write(new String("PASS "+s+"\n").getBytes());
+		
+		if(!read("230")){
+			return false;
+		}
+		
+		return true;
+	}
+	
+
+	/**
+	 * Commande QUIT
+	 * Envoie QUIT au serveur FTP. 
+	 * Commande finale
+	 * @param s Chaine vide.
+	 */
 	public void CMDQUIT(String s) throws IOException{
 		out.write(new String("QUIT\n").getBytes());
 	}
 
+	
+	/**
+	 * Commande liste
+	 * Envoie LIST au serveur FTP.
+	 * Lorsqu'on gère des fichiers, on doit ouvrir une seconde socket.
+	 * Le client accepte la connexion à cette socket. 
+	 * Attend de lire 226
+	 * Terminal : ls
+	 * @param s Nom du repertoire
+	 */
 	public  String CMDLIST(String file) throws IOException{
-		ServerSocket server =CMDPORT("127,0,0,1");
+		
+		/*
+		 * 
+		 */
+		ServerSocket server = CMDPORT("127,0,0,1");
 		out.write(new String("LIST\n").getBytes());
 		read("150");
 		Socket soc = server.accept();
@@ -98,7 +158,16 @@ public class FTPCommandes {
 
 
 
-
+	/**
+	 * Commande RETR.
+	 * Envoie RETR au serveur FTP. 
+	 * Lorsqu'on gère des fichiers, on doit ouvrir une seconde socket.
+	 * Le client accepte la connexion à cette socket. 
+	 * Attend de lire 226
+	 * Terminal : get
+	 * Le client lit ce que le serveur envoie et l'écrit dans un fichier.
+	 * @param s nom du fichier a recuperer
+	 */
 	public boolean CMDRETR(String file) throws IOException{
 		ServerSocket server =CMDPORT("127,0,0,1");
 		out.write(new String("RETR " +file+"\n").getBytes());
@@ -131,7 +200,14 @@ public class FTPCommandes {
 		return true;
 	}
 
-	
+	/**
+	 * Commande DELE
+	 * Envoie DELE au serveur FTP. 
+	 * Attend de lire 226
+	 * Terminal : delete
+	 * Le client lit ce que le serveur envoie et l'écrit dans un fichier.
+	 * @param s Nom de l'utilisateur
+	 */
 	public boolean CMDDELE(String file) throws IOException{
 		
 		out.write(new String("DELE " +file+"\n").getBytes());
@@ -142,6 +218,16 @@ public class FTPCommandes {
 		return true;
 	}
 
+	/**
+	 * Commande STOR.
+	 * Envoie STOR au serveur FTP. 
+	 * Lorsqu'on gère des fichiers, on doit ouvrir une seconde socket.
+	 * Le client accepte la connexion à cette socket. 
+	 * Attend de lire 226
+	 * Terminal : put
+	 * Le client envoie au serveur le fichier.
+	 * @param s Nom du fichier à envoyer
+	 */
 	public boolean CMDSTOR(String file) throws IOException{
 		ServerSocket server =CMDPORT("127,0,0,1");
 		out.write(new String("STOR " +file+"\n").getBytes());
@@ -175,10 +261,19 @@ public class FTPCommandes {
 		
 	}
 
+	
+	/**
+	 * Commande PORT
+	 * Utile pour les commandes STOR, RETR et LIST
+	 * Attend de lire 225
+	 * Pour obtenir le numéro du port,
+	 * on utilise un chiffre aléatoire que l'on multiplie par 256 et auquel on ajoute 42.
+	 * @param s adresse
+	 */
 	public ServerSocket CMDPORT(String addr) throws IOException{
 
-		Random r =new Random();
-		int port =r.nextInt(200);
+		Random r = new Random();
+		int port = r.nextInt(200);
 
 		out.write(new String("PORT "+addr+","+port+",42\n").getBytes());
 		read("225");	
@@ -187,8 +282,16 @@ public class FTPCommandes {
 		port += 42;
 
 		return new ServerSocket(port);
-
 	}
+	
+	/**
+	 * Commande CWD.
+	 * Envoie CWD au serveur FTP. 
+	 * Attend de lire 250
+	 * Terminal : cd
+	 * Le client lit ce que le serveur envoie et l'écrit dans un fichier.
+	 * @param s Nom du chemin
+	 */
 	public boolean CMDCWD(String path) throws IOException{
 
 		String newPath;
@@ -209,6 +312,10 @@ public class FTPCommandes {
 
 	}
 	
+	/**
+	 * Accesseur au répertoire courant
+	 * @return répertoie courant
+	 */
 	public String getCurrentDir(){
 		return this.currentLoc;
 	}
